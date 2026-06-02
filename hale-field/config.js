a9ba6b867b40beac724c9910ae7cdd04a3cbb34b
pete2786 <eazydp@gmail.com>
2026-06-02 08:50:52 -0500

    Wire Apps Script submit endpoints into both pool configs (verified end-to-end)

--- hale-field/config.js
+++ hale-field/config.js
@@ -2,7 +2,7 @@
 // After publishing this pool's two Leaderboard tabs to CSV, paste those URLs below.
 window.POOL = {
   label:            "Hale Field",
-  submitUrl:        "",            // Apps Script web-app /exec URL for one-tap submit (group + bracket). Blank = players use Copy code. See SETUP.md.
+  submitUrl:        "https://script.google.com/macros/s/AKfycbxOeIMPwO_kLzgroH-2O0Av7-X6EsXSsiknwfqvcR4yoeH3m9g6iDXm-2J-ijwMeQIXtQ/exec", // Apps Script one-tap submit (group + bracket)
   bracketSubmitUrl: "",            // optional; falls back to submitUrl if blank
   groupCsvUrl:      "https://docs.google.com/spreadsheets/d/e/2PACX-1vRbGs7rnyfUFQtO0hBpVhQSnbQav5SR4jpkxkskm8l_rjeImnL0dhTJClJstjQeIIWPpeuLD3z-XyMy/pub?gid=1069705092&single=true&output=csv",
   knockoutCsvUrl:   "https://docs.google.com/spreadsheets/d/e/2PACX-1vRbGs7rnyfUFQtO0hBpVhQSnbQav5SR4jpkxkskm8l_rjeImnL0dhTJClJstjQeIIWPpeuLD3z-XyMy/pub?gid=825300602&single=true&output=csv",
--- justifi/config.js
+++ justifi/config.js
@@ -2,7 +2,7 @@
 // After publishing this pool's two Leaderboard tabs to CSV, paste those URLs below.
 window.POOL = {
   label:            "Justifi",
-  submitUrl:        "",
+  submitUrl:        "https://script.google.com/macros/s/AKfycbyi8vuloYbSy8gTK74eWB4qubp0ov1nE2GlEkfGNX9SE8aZExLPZccvTDv2i5RvEGCG/exec",
   bracketSubmitUrl: "",
   groupCsvUrl:      "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ4IvSJa5miZlvtq76WVC-wxf2c-Oyd9S-riiuvJZ1t08DAXYzVd4DtT3jzlMOx6zlJ0Cv350BV4Vrh/pub?gid=1069705092&single=true&output=csv",
   knockoutCsvUrl:   "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ4IvSJa5miZlvtq76WVC-wxf2c-Oyd9S-riiuvJZ1t08DAXYzVd4DtT3jzlMOx6zlJ0Cv350BV4Vrh/pub?gid=825300602&single=true&output=csv",
